c97a6f23d5051c8d5014139e8604eefc6f41bcb0
lollix89 <lollix89@gmail.com>
2013-11-13 17:21:17 +0100

    linear function

--- computePosteriorAndMutualInfo.m
+++ computePosteriorAndMutualInfo.m
@@ -1,5 +1,5 @@
 function [prior, posterior, mutualInfo]= computePosteriorAndMutualInfo(prior, posterior, mutualInfo, temperatureVector, FValues, X, Y, pointX, pointY, range)
-sill= 1;
+sill= 5;
 for i=1: size(X,1)
     [~, closestValueIndex] = min(abs(temperatureVector- FValues(i)));
     
@@ -7,12 +7,13 @@ for i=1: size(X,1)
         for y_=1:size(pointY,2)
             
             currentDistance= pdist([X(i) Y(i); pointX(x_) pointY(y_)]);
-            
-            if currentDistance <= range
-                varianceFunction= .01 + (sill*(1.5*(currentDistance/range)-.5*(currentDistance/range)^3));
-            else
-                varianceFunction=  .01+ sill+ 4*(sill/range)*(currentDistance-range);
-            end
+%             
+%             if currentDistance <= range
+%                 varianceFunction= .01 + (sill*(1.5*(currentDistance/range)-.5*(currentDistance/range)^3));
+%             else
+%                 varianceFunction=  .01+ sill+ 4*(sill/range)*(currentDistance-range);
+%             end
+            varianceFunction= .01 + sill*(currentDistance/range);
             
             likelihoodCurrentCell= pdf('norm', temperatureVector, temperatureVector(closestValueIndex), varianceFunction);
             likelihoodCurrentCell= likelihoodCurrentCell./sum(likelihoodCurrentCell);
