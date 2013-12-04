4838b757fe6ec0c48d2036d543483e2abe3ebef9
lollix89 <lollix89@gmail.com>
2013-12-04 09:05:07 +0100

    performance improvements

--- sim-code/+mutual/computePosteriorAndMutualInfo.m
+++ sim-code/+mutual/computePosteriorAndMutualInfo.m
@@ -25,6 +25,12 @@ if isKey(qrs.config,'Sill')
 else
     sill= 5;
 end
+if isKey(qrs.config,'Function') && strcmp(qrs.config('Function'), 'sph')
+    func= 'spherical';
+elseif (isKey(qrs.config,'Function') && strcmp(qrs.config('Function'), 'lin')) || ~isKey(qrs.config,'Function')
+    func= 'linear';
+end
+
 
 for i=1: size(coords,1)
     [~, closestValueIndex] = min(abs(temperatureV- samples(i)));
@@ -34,13 +40,13 @@ for i=1: size(coords,1)
             
             currentDistance= sqrt(sum(([coords(i,2) coords(i,1)]- [(((rows-1)*delta)+delta/2) (((cols-1)*delta)+delta/2)]).^2));
             
-            if isKey(qrs.config,'Function') && strcmp(qrs.config('Function'), 'sph')
+            if strcmp(func, 'spherical')
                 if currentDistance <= range
                     sigma_= .01 + (sill*(1.5*(currentDistance/range)-.5*(currentDistance/range)^3));
                 else
                     sigma_= .01+ sill;
                 end
-            elseif (isKey(qrs.config,'Function') && strcmp(qrs.config('Function'), 'lin')) || ~isKey(qrs.config,'Function')
+            elseif strcmp(func, 'linear')
                 sigma_= .01 + currentDistance*(sill/range);
             end
             likelihoodCurrentCell= exp(-0.5 * ((temperatureV - temperatureV(closestValueIndex))./sigma_).^2) ./ (sqrt(2*pi) .* sigma_);
